871ec46b43f46d27f02457b0b53ba9ad41737ad0
ahyeonjoung <ribbon0529@naver.com>
2021-03-16 19:44:17 +0900

    Create 2018_n진수게임.cpp

---
+++ 2018_n진수게임.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+ char number[18] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                   'A', 'B', 'C', 'D', 'E', 'F'};
+
+string convert(int num, int n){
+    string st;
+    if(num == 0){
+        return "0";
+    }
+    while(num > 0){
+        st += number[num % n];
+        num /= n;
+    }
+    reverse(st.begin(), st.end());
+    return st;
+}
+
+
+string solution(int n, int t, int m, int p) {
+    string answer = "";   
+    string temp="";
+    for(int num = 0; temp.size() <= m*t; num++){
+        string ngame = convert(num, n);
+        temp += ngame;
+    }
+    
+    for(int i = 0; i < t; i++){
+        answer += temp.at((m*i)+(p-1));
+    }
+    return answer;
+}
