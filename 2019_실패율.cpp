5cc2469a7bc9b97cbae23f26974f2b81868dd0db
정아연 <55370149+ahyeonjoung@users.noreply.github.com>
2021-03-30 00:09:19 +0900

    Update 2019_실패율.cpp

--- 2019_실패율.cpp
+++ 2019_실패율.cpp
@@ -1,49 +1,44 @@
 #include <string>
 #include <vector>
-#include <algorithm>  
-#include<iostream>
-
 using namespace std;
 
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
-    answer.assign(N, 0);
-    int tmp = 0;
+    answer.assign(N,0);
+    int tmp=0;
     vector <int> correct;
-    correct.assign(N + 1, 0);
+    correct.assign(N+1,0);
     vector <int> trying;
-    trying.assign(N + 1, 0);
+    trying.assign(N+1,0);
     vector <double> temp;
-    temp.assign(N + 1, 0.0);
-
-    for (int i = 0; i < stages.size(); i++) {
-        tmp = stages[i];
-        for (int j = 0; j < tmp; j++) {
+    temp.assign(N+1,0.0);
+    
+    for(int i=0;i<stages.size();i++){
+        tmp=stages[i];
+        for(int j=0;j<tmp;j++){
             trying[j]++;
         }
-        if (tmp != N + 1)
-            correct[tmp - 1]++;
+        if(tmp!=N+1)
+            correct[tmp-1]++;
     }
-
-    for (int i = 0; i < N; i++) {
-        if (trying[i] != 0)
-            temp[i] = (double)correct[i] / (double)trying[i];
-        answer[i] = i + 1;
-
+    
+    for(int i=0;i<N;i++){
+        if(trying[i]!=0)
+            temp[i]=(double)correct[i]/(double)trying[i];
+        answer[i]=i+1;
     }
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N - 1 - i; j++) {
-            if (temp[j] < temp[j + 1]) {
-                double t = temp[j];
-                temp[j] = temp[j + 1];
-                temp[j + 1] = t;
-                int k = answer[j];
-                answer[j] = answer[j + 1];
-                answer[j + 1] = k;
+    
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N-1-i;j++){
+            if(temp[j]<temp[j+1]){
+                double t=temp[j];
+                temp[j]=temp[j+1];
+                temp[j+1]=t;
+                int k=answer[j];
+                answer[j]=answer[j+1];
+                answer[j+1]=k;
             }
         }
     }
-
     return answer;
 }
