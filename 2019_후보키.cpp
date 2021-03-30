fdca564d9b2a84c64a24ba2dc67d66e1ff46d52c
ahyeonjoung <ribbon0529@naver.com>
2021-03-30 17:52:08 +0900

    Create 2019_후보키.cpp

---
+++ 2019_후보키.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include<set>
+using namespace std;
+
+bool possi(vector<int> vec, int now) {
+    for (int i = 0; i < vec.size(); i++)
+        // ans에 있던 조합이 이번에 들어온 조합이랑 같은 것인지 확인
+        if ((vec[i] & now) == vec[i])
+            return false;
+
+    return true;
+}
+
+int solution(vector<vector<string>> relation) {
+    int rowSize = (int)relation.size();
+    int colSize = (int)relation[0].size();
+    vector<int> ans;
+    for (int i = 1; i < (1 << colSize); i++) {
+        set<string> s;
+
+        for (int j = 0; j < rowSize; j++) {
+            string now = "";
+
+            for (int k = 0; k < colSize; k++) {
+                // 선택된 컬럼과 일치한다면
+                if (i & (1 << k))
+                    now += relation[j][k];
+            }
+            s.insert(now);
+        }
+
+        if (s.size() == rowSize && possi(ans, i))
+            ans.push_back(i);
+    }
+
+    return (int)ans.size();
+}
