71dd333160f15071cf1eab38948c723f38e8dbc6
정아연 <55370149+ahyeonjoung@users.noreply.github.com>
2021-08-29 23:33:00 +0900

    Update 2019_실패율.cpp

--- cpp/2019_실패율.cpp
+++ cpp/2019_실패율.cpp
@@ -1,6 +1,7 @@
 #include <string>
-#include <vector> 
-using namespace std; ;
+#include <vector>     
+using namespace std;
+
 
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
