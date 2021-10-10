a5886de6591dac938a427294e2ed19ddc7933c3e
정아연 <ribbon0529@ewhain.net>
2021-10-10 23:55:13 +0900

    Update 완주하지 못한선수.py

--- python/완주하지 못한선수.py
+++ python/완주하지 못한선수.py
@@ -3,10 +3,12 @@ def solution(participant, completion):
     counter=Counter(completion)
     counter2=Counter(participant)
     answer=''
-    
+   
+ 
     for word in participant:
         if counter[word]==0:
             answer=word
         if counter[word]-counter2[word]!=0:
             answer=word
     return answer
+
