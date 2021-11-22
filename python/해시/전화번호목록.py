144e107b67658849bf35d2ca144a701b5a699fc8
ahyeonjoung <ribbon0529@naver.com>
2021-11-22 23:17:25 +0900

    Create 전화번호목록.py

---
+++ python/해시/전화번호목록.py
@@ -0,0 +1,9 @@
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    #만약 접두어가 존재한다면, 정렬했을때 앞뒤로 올수밖에 없다. 
+    for i in range(0,len(phone_book)-1):
+        if phone_book[i+1].find(phone_book[i],0, len(phone_book[i]))==0:
+            answer=False
+            return answer
+    return answer
