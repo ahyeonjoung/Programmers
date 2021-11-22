f341303224c6c786ca362e390704e2f64ae11738
ahyeonjoung <ribbon0529@naver.com>
2021-11-22 22:43:35 +0900

    add 완주하지못한선수.py

---
+++ python/해시/완주하지못한선수.py
@@ -0,0 +1,12 @@
+from collections import Counter
+def solution(participant, completion):
+    counter=Counter(completion)
+    counter2=Counter(participant)
+    answer=''
+    
+    for word in participant:
+        if counter[word]==0:
+            answer=word
+        if counter[word]-counter2[word]!=0:
+            answer=word
+    return answer
