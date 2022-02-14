c5a670e3d34f2b85f6ed34c9bf0cce22f95e6921
ahyeonjoung <ribbon0529@naver.com>
2022-02-14 20:48:23 +0900

    add 2018_숫자게임

---
+++ python/2018_숫자게임.py
@@ -0,0 +1,16 @@
+
+def solution(A, B):
+    answer = 0
+    A.sort(reverse=True)
+    B.sort(reverse=True)
+    j=0
+    for i in range(len(A)):
+        if A[i]>=B[j]:
+            continue
+        else:
+            j+=1
+    answer=(j)
+
+
+
+    return answer
