3ab6101d98717a59ebda9f8f22061334a25a5fcb
ahyeonjoung <ribbon0529@naver.com>
2022-02-14 20:44:54 +0900

    add 2018_스킬트리

---
+++ python/2018_배달.py
@@ -0,0 +1,35 @@
+import heapq
+from collections import defaultdict
+def dijkstra(graph,start):
+    distances={node: float('inf') for node in graph}
+    distances[start]=0
+    queue=[]
+    heapq.heappush(queue,[distances[start],start])
+    while queue:
+        current_distance, current_destination=heapq.heappop(queue)
+        if distances[current_destination]<current_distance:
+            continue
+        for new_destination, new_distance in graph[current_destination]:
+            distance=current_distance+new_distance
+            if distance<distances[new_destination]:
+                distances[new_destination]=distance
+                heapq.heappush(queue,[distance,new_destination])
+    return distances
+
+def solution(N, road, K):
+    answer = 0
+    result=[]
+    board=defaultdict(list)
+    for a,b,c in road:
+        board[str(a)].append([str(b),c])
+        board[str(b)].append([str(a),c])
+    result=dijkstra(board,'1')
+
+    for i in result.values():
+        if i<=K:
+            answer+=1
+
+
+
+
+    return answer
---
+++ python/2018_스킬트리.py
@@ -0,0 +1,17 @@
+def solution(skill, skill_trees):
+    answer = 0
+    for skills in skill_trees:
+        stack=list(skill)
+        stack.reverse()
+        flag=True
+        for s in list(skills):
+            if s in stack:
+                if stack[-1]!=s:
+                    flag=False
+                    break
+                else:
+                    stack.pop()
+        if flag:
+            answer+=1
+
+    return answer
