2d1ddd07c610e59ad6f8c471591a7d75a3efa253
ahyeonjoung <ribbon0529@naver.com>
2022-04-15 10:07:07 +0900

    ADD 2018_캐시

---
+++ python/2018_캐시.py
@@ -0,0 +1,20 @@
+from collections import deque
+def solution(cacheSize, cities):
+    answer = 0
+    q=deque()
+    if cacheSize==0:
+        return 5*len(cities)
+    for city in cities:
+        city=city.lower()
+        if city in q:
+            answer+=1
+            q.remove(city)
+            q.append(city)
+        else:
+            if len(q)<cacheSize:
+                q.append(city)
+            else:
+                q.popleft()
+                q.append(city)
+            answer+=5
+    return answer
